0ae58310bd6bb57896b4368adec3a040c21a1b78
Erika Pauwels <erika.pauwels@gmail.com>
2025-09-25 15:07:16 +0200

    Refactor login endpoint

--- app.js
+++ app.js
@@ -6,52 +6,6 @@ import { DELTA_INTERVAL, LOG_INCOMING_DELTA, KEY } from './env-config';
 const cache = new DeltaCache();
 let hasTimeout = null;
 
-//TODO: Re-consider the location of this endpoint.
-// The session created here, is used in other services too.
-app.post('/login', async function(req, res) {
-  try {
-
-    // 0. To avoid false sense of security, login only makes sense if accepted key is configured
-    if (!KEY) {
-      throw 'No key configured in service.';
-    }
-
-    // 1. get environment info
-    const sessionUri = req.get('mu-session-id');
-
-    // 2. validate credentials
-    if (req.get('key') !== KEY) {
-      throw 'Key does not match';
-    }
-
-    // 3. add new login to session
-    update(`PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
-      INSERT DATA {
-        GRAPH <http://mu.semte.ch/graphs/diff-producer/login> {
-          ${sparqlEscapeUri(sessionUri)} muAccount:account <http://services.lblod.info/diff-consumer/account>.
-        }
-      }`, { sudo: true });
-
-    // 4. request login recalculation
-    return res
-      .header('mu-auth-allowed-groups', 'CLEAR')
-      .status(201)
-      .send({
-        links: {
-          self: '/sessions/current'
-        },
-        data: {
-          type: 'sessions',
-          id: uuid()
-        }
-      });
-  }
-  catch (e) {
-    console.error(e);
-    return res.status(500).send({ message: 'Something went wrong' });
-  }
-});
-
 app.post('/delta', bodyParser.json({ limit: '500mb' }), async function( req, res ) {
   const delta = req.body;
 
@@ -76,6 +30,42 @@ app.get('/files', async function( req, res ) {
   const since = req.query.since || new Date().toISOString();
   const files = await cache.getDeltaFiles(since);
   res.json({ data: files });
+});
+
+app.post('/login', async function(req, res, next) {
+  try {
+    if (!KEY) {
+      next(new Error('No key configured in service'));
+    } else {
+      const sessionUri = req.get('mu-session-id');
+
+      if (req.get('key') !== KEY) {
+        next(new Error('Invalid key'));
+      } else {
+        update(`PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
+          INSERT DATA {
+            GRAPH <http://mu.semte.ch/graphs/diff-producer/login> {
+              ${sparqlEscapeUri(sessionUri)} muAccount:account <http://services.lblod.info/diff-consumer/account>.
+          }
+        }`, { sudo: true });
+
+        return res.header('mu-auth-allowed-groups', 'CLEAR').status(201).send({
+          links: {
+            self: '/sessions/current'
+          },
+          data: {
+            type: 'sessions',
+            id: uuid()
+          }
+        });
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    const error = new Error('Something went wrong')
+    error.status = 500;
+    next(error);
+  }
 });
 
 function triggerTimeout(){
